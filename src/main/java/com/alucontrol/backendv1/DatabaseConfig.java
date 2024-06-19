b5d02c9c91084f172e610a7ca0afea84588b86d1
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-19 12:33:31 +0100

    Create DatabaseConfig.java

---
+++ src/main/java/com/alucontrol/backendv1/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package com.alucontrol.backendv1;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+//https://devcenter.heroku.com/articles/deploying-spring-boot-apps-to-heroku
+@Configuration
+public class DatabaseConfig
+{
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
