cc3a74db9303986212d05c3b8b62a78ad6e09578
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-19 12:21:29 +0100

    ApplicationRunner added

--- src/main/java/com/alucontrol/backendv1/Backendv1Application.java
+++ src/main/java/com/alucontrol/backendv1/Backendv1Application.java
@@ -10,14 +10,24 @@
  */
 package com.alucontrol.backendv1;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Backendv1Application {
-
+public class Backendv1Application
+{
     public static void main(String[] args) {
         SpringApplication.run(Backendv1Application.class, args);
     }
 
+    @Bean
+    public ApplicationRunner applicationRunner(ServerProperties serverProperties) {
+    return args -> {
+            int port = Integer.parseInt(System.getenv("PORT"));
+            System.out.println("Server is running on port " + port);
+        };
+    }
 }
