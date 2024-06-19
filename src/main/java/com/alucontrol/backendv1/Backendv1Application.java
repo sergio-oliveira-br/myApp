eb77e43caa29f2300ec08f4438403a275e4aa13a
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-19 12:29:44 +0100

    Update Backendv1Application.java

--- src/main/java/com/alucontrol/backendv1/Backendv1Application.java
+++ src/main/java/com/alucontrol/backendv1/Backendv1Application.java
@@ -10,28 +10,22 @@
  */
 package com.alucontrol.backendv1;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Backendv1Application
 {
-    @Value("${server.port}")
-    private int port;
-
     public static void main(String[] args) {
         SpringApplication.run(Backendv1Application.class, args);
     }
 
     @Bean
-    public ApplicationRunner applicationRunner(ServerProperties serverProperties) {
-    return args -> {
-            int port = Integer.parseInt(System.getenv("PORT"));
-            System.out.println("Server is running on port " + port);
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            System.out.println("Server is running on port " + System.getenv("PORT"));
         };
     }
 }
