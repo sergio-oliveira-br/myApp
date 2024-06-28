7d7f4c2e96c30d447a8b705cfa8e57cad09883ff
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-28 16:54:44 +0100

    Create ExpenseCreateUpdateController.java
    This controller is dedicated to endpoints that create and update records

---
+++ src/main/java/com/alucontrol/backendv1/Controllers/Expense/ExpenseCreateUpdateController.java
@@ -0,0 +1,27 @@
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie
+ * Project Commencing May 2024
+ * Version: 1.0
+ */
+package com.alucontrol.backendv1.Controllers.Expense;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+/** This controller is dedicated to endpoints that create and update records
+ * It is the responsibility of this layer to receive requests, call methods from the service layer, and return HTTP responses */
+@RestController
+public class ExpenseCreateUpdateController
+{
+    //Repository for access to expense data
+
+    //Constructor responsible for injecting the repository
+
+    /** Endpoints */
+
+}
