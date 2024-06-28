b34927144f7c95de4e63b05fda46c2eeb32f8020
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-28 20:41:33 +0100

    Create expensescript.js

---
+++ src/main/resources/static/js/expensescript.js
@@ -0,0 +1,22 @@
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie
+ * Project Commencing May 2024
+ * Version: 1.0
+ */
+
+/** Load content when the page is loaded */
+$(document).ready(function () {
+
+})
+
+
+/**
+ Page: Expense
+ Item: Table
+ Method: The script will load all expenses in a table
+ */
