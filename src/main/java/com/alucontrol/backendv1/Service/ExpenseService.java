09c47fc99af451a7c486599e9504b1a779f1ba24
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-28 22:22:02 +0100

    Update ExpenseService.java
    createExpense() -> This method will allow the user create a new expense

--- src/main/java/com/alucontrol/backendv1/Service/ExpenseService.java
+++ src/main/java/com/alucontrol/backendv1/Service/ExpenseService.java
@@ -12,7 +12,11 @@ package com.alucontrol.backendv1.Service;
 
 import com.alucontrol.backendv1.Model.Expense;
 import com.alucontrol.backendv1.Repository.ExpenseRepository;
+import com.alucontrol.backendv1.Util.DateUtil;
+import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /** This Service Class has methods that contain business logic.*/
 @Service
@@ -27,10 +31,36 @@ public class ExpenseService
         this.expenseRepository = expenseRepository;
     }
 
-    /** This method will allow the user create a new expense */
-    public Expense createExpense() throws Exception
+    /** Used: Expense Page through the Controller
+     *  Method: This method will allow the user create a new expense */
+    public Expense createExpense(String expenseDescription, Double expenseAmount,
+                                 String expenseDate,String expenseCategory,
+                                 String expenseAdditionalNotes) throws Exception
     {
+        try {
+            //Creates a new instance of Expense
+            Expense expense = new Expense();
 
+            //Set the values
+            expense.setExpenseDescription(expenseDescription);
+            expense.setExpenseAmount(expenseAmount);
+            expense.setExpenseDate(expenseDate);
+            expense.setExpenseCategory(expenseCategory);
+            expense.setExpenseAdditionalNotes(expenseAdditionalNotes);
+
+            //Create a log
+            LoggerUtil.info("Expense created" + expense.getId());
+
+            //Saves the new expense in the repository and returns it.
+            return expenseRepository.save(expense);
+        }
+        catch (Exception e)
+        {
+            //Create a log (if there is an error)
+            LoggerUtil.info("Error creating expense");
+
+            //Throw the exception (if there is an error)
+            throw new Exception(e.getMessage());
+        }
     }
-
 }
