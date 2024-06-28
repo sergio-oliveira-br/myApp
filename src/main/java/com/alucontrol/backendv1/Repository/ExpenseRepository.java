711fd03c8cd448116ef601347ee3e8469311a39c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-06-28 16:56:29 +0100

    Create ExpenseRepository.java
    This is responsible for managing the persistence of expenses data

---
+++ src/main/java/com/alucontrol/backendv1/Repository/ExpenseRepository.java
@@ -0,0 +1,22 @@
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie
+ * Project Commencing May 2024
+ * Version: 1.0
+ */
+package com.alucontrol.backendv1.Repository;
+
+import org.springframework.stereotype.Repository;
+
+/** This is responsible for managing the persistence of Expense data
+ *  in the database, providing a simplified interface for CRUD operations
+ */
+@Repository
+public interface ExpenseRepository
+{
+
+}
