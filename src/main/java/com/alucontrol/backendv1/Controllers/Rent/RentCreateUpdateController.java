69f5717100adb65581b54fabdc6ac89f1db617f4
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-07-31 11:18:38 +0100

    log added to capture all data

--- src/main/java/com/alucontrol/backendv1/Controllers/Rent/RentCreateUpdateController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Rent/RentCreateUpdateController.java
@@ -62,6 +62,7 @@ public class RentCreateUpdateController
             }
 
             LoggerUtil.info("Save Order Successfully, ID:" + savedRent.getId() + ", " + savedRent.getRentFirstName());
+            LoggerUtil.info("New Rent data: " + savedRent.toString());
             return ResponseEntity.ok(savedRent);
 
         }
