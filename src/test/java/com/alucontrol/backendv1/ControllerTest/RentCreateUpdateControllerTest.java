094bf2ed01a7ed3423fb5457e37347857a655c58
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-07-31 14:21:00 +0100

    The tests has been sorted

--- src/test/java/com/alucontrol/backendv1/ControllerTest/RentCreateUpdateControllerTest.java
+++ src/test/java/com/alucontrol/backendv1/ControllerTest/RentCreateUpdateControllerTest.java
@@ -16,7 +16,6 @@
 //import com.alucontrol.backendv1.Controllers.Rent.RentCreateUpdateController;
 //import com.alucontrol.backendv1.Model.Rent;
 //import com.alucontrol.backendv1.Repository.RentRepository;
-//import com.alucontrol.backendv1.Service.RentService;
 //import org.junit.jupiter.api.Test;
 //import org.junit.jupiter.api.extension.ExtendWith;
 //import org.mockito.InjectMocks;
@@ -48,9 +47,9 @@
 //
 //    //Create a Mock Instance of the RentRepository
 //    @Mock
-//    public RentService rentService;
+//    public RentRepository rentRepository;
 //
-//    //The success case: where the product is saved correctly in DB
+//    //The success case: where the rent is saved correctly in DB
 //    @Test
 //    public void saveRentSuccess()
 //    {
@@ -73,48 +72,42 @@
 //        /** Stubbing: Set the behavior of the Repository save method.
 //         * When the save method is called in the rentRepository object
 //         * with the rent argument, return the rent itself as a result */
-//        try {
-//            when(rentService.createRent(any(String.class), any(String.class), any(String.class), any(double.class),
-//                    any(Integer.class), any(String.class), any(String.class), any(Integer.class), any(double.class),
-//                    any(String.class), any(String.class), any(String.class))).thenReturn(rent);
-//        } catch (ParseException e) {
-//            throw new RuntimeException(e);
-//        }
-//
+//        when(rentRepository.save(any(Rent.class))).thenReturn(rent);
 //
 //        ResponseEntity<Rent> response;
-//        response = rentCreateUpdateController.saveRent("RentTest99", "AddressTest", "ItemTest", 99,
-//                99, "01/01/2024", "02/01/2024", 99, 99, "This is a JUnitTest",
-//                "PaidTest", "NewTest");
+//        response = rentCreateUpdateController.saveRent(rent);
 //
-//        /** The method is expected to return a response with a status code of 200 (OK) and the client saved */
+//        /** The method is expected to return a response with a status code of 200 (OK) and the rent saved */
 //        assertEquals(HttpStatus.OK, response.getStatusCode());
 //        assertEquals(rent, response.getBody());
-//        try {
-//            verify(rentService).createRent(any(String.class), any(String.class), any(String.class), any(double.class),
-//                    any(Integer.class), any(String.class), any(String.class), any(Integer.class), any(double.class),
-//                    any(String.class), any(String.class), any(String.class));
-//        } catch (ParseException e) {
-//            throw new RuntimeException(e);
-//        }
+//        verify(rentRepository).save(any(Rent.class));
 //    }
 //
 //    //The error case, where an exception is thrown when trying to save the product to the database.
 //    @Test
 //    public void saveRentError() throws Exception
 //    {
+//        //Create a instance object
+//        Rent rent = new Rent();
+//
+//        rent.setRentFirstName("RentTest");
+//        rent.setRentAddress("AddressTest");
+//        rent.setRentItem("ItemTest");
+//        rent.setRentPrice(99);
+//        rent.setRentQtyItem(99);
+//        rent.setRentStarts("01/01/2024");
+//        rent.setRentEnds("02/01/2024");
+//        rent.setRentTotalDays(99);
+//        rent.setRentTotalPrice(99);
+//        rent.setRentDetails("This is a JUnitTest");
+//        rent.setRentPaymentStatus("PaidTest");
+//        rent.setRentStatus("NewTest");
 //
 //        //Mockito will throw an Exception instead of actually saving the rent in to the database
-//        when(rentService.createRent(any(String.class), any(String.class), any(String.class), anyDouble(),
-//                anyInt(), any(String.class), any(String.class), anyInt(), anyDouble(),
-//                any(String.class), any(String.class), any(String.class)))
-//                .thenThrow(new ParseException("Error parsing dates", 0));
-//
+//        when(rentRepository.save(any(Rent.class))).thenThrow(new ParseException("An error occurred while trying to save.", 0));
 //
 //        ResponseEntity<Rent> response;
-//        response = rentCreateUpdateController.saveRent("RentTest99", "AddressTest", "ItemTest", 99,
-//                99, "01/01/2024", "02/01/2024", 99, 99, "This is a JUnitTest",
-//                "PaidTest", "NewTest");
+//        response = rentCreateUpdateController.saveRent(rent);
 //
 //        //Checks whether the server response is an internal error (500) and whether the response body is empty
 //        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
@@ -122,5 +115,4 @@
 //        //Checks if the response body is empty, as there is nothing to be returned in case of error
 //        assertNull(response.getBody());
 //    }
-//
 //}
