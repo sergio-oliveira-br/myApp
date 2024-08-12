38f19208213d1c63f2da274558d6ba318fa29015
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-08-12 20:00:08 +0100

    Update ItemPriceProjection.java
    -> This projection is used in the Sales and Rent FORM, loading the description and price

--- src/main/java/com/alucontrol/backendv1/Projection/Product/ItemPriceProjection.java
+++ src/main/java/com/alucontrol/backendv1/Projection/Product/ItemPriceProjection.java
@@ -1,4 +1,17 @@
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie
+ * Project Commencing May 2024
+ * Version: 2.0
+ */
 package com.alucontrol.backendv1.Projection.Product;
 
+//This projection is used in the Sales and Rent FORM, loading the description and price
 public interface ItemPriceProjection {
+    String getItemDescription();
+    Double getItemPrice();
 }
