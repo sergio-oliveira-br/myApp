01e1e2d68a207ffb199391fe1cf433a5e34e2d8d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-08-12 18:49:01 +0100

    method to display the list of all clients
    feat(customer): load customer list on page load
    
    Adds functionality to load a list of customers from the server and populate the customer select element on page load.
    
    Uses AJAX to fetch customer data and dynamically creates option elements for each customer.

---
+++ src/main/resources/static/js/sales.js
@@ -0,0 +1,41 @@
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie
+ * Project Commencing May 2024
+ * Version: 2.0
+ */
+
+/**
+ Method: When the page is loaded or reloaded, the methods here will be updated
+ */
+$(document).ready(function () {
+    //load the available customer
+    loadCustomerList();
+
+})
+
+
+/**
+ Method: Create a list of all clients and display it in the client field of the form
+ */
+function loadCustomerList(){
+    ajaxRequest("/customer", function (data){
+        //variable
+        let saleCustomerSelect = $('#saleCustomer');
+
+        //cleaning to display each item only once
+        saleCustomerSelect.empty();
+
+        //Iteration
+        data.forEach(function(customer){
+            saleCustomerSelect.append('<option value="' + customer.firstName +  + customer.lastName + " - " + customer.phoneNumber + '">' +
+                customer.firstName + " " + customer.lastName + " - " + customer.phoneNumber +'</option>');
+        });
+    });
+}
+
+
