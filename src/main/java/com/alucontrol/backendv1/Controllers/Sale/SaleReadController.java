487b1b42da6a77ab3fb85b4554555d201b8578bd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-08-15 13:12:26 +0100

    Endpoint to retrieve all sales created
    retrieve data by CRUD approach

--- src/main/java/com/alucontrol/backendv1/Controllers/Sale/SaleReadController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Sale/SaleReadController.java
@@ -12,8 +12,10 @@ package com.alucontrol.backendv1.Controllers.Sale;
 
 import com.alucontrol.backendv1.Exception.ResourceNotFoundException;
 import com.alucontrol.backendv1.Model.Product;
+import com.alucontrol.backendv1.Model.Sale;
 import com.alucontrol.backendv1.Projection.Product.ItemPriceProjection;
 import com.alucontrol.backendv1.Repository.ProductRepository;
+import com.alucontrol.backendv1.Repository.SaleRepository;
 import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,11 +28,26 @@ import java.util.List;
 public class SaleReadController {
     //Repository for access to product data
     private final ProductRepository productRepository;
+    private final SaleRepository saleRepository;
 
     //Constructor responsible for injecting the repository
-    public SaleReadController(ProductRepository productRepository) {
+    public SaleReadController(ProductRepository productRepository, SaleRepository saleRepository) {
         this.productRepository = productRepository;
+        this.saleRepository = saleRepository;
     }
 
+    /** Endpoint to retrieve all sales */
+    @GetMapping("/sale")
+    public ResponseEntity<List<Sale>> getAllSales() {
+        //Log
+        LoggerUtil.info("Looking for all sales records.");
 
+        //Retrieve the data by CRUD
+        List<Sale> sale = saleRepository.findAll();
+
+        //Log
+        LoggerUtil.info("Fetched " + sale.size() + " sales records"); //Create a log
+
+        return ResponseEntity.ok(sale);
+    }
 }
--- src/main/resources/static/js/sales.js
+++ src/main/resources/static/js/sales.js
@@ -19,6 +19,9 @@ $(document).ready(function () {
     //load the items witch are classified as "Vendas" em productType
     loadProductListByProductType("Venda")
 
+    //render the table
+    loadSales();
+
 })
 
 
@@ -104,5 +107,35 @@ document.getElementById('saleItem').addEventListener('change', loadTotalPriceSal
 document.getElementById('saleQtyItem').addEventListener('change', loadTotalPriceSales);
 
 
+/**
+ Page: Sale - Table
+ Method: The script will load all items in a table
+ */
+function loadSales()
+{
+    //Call the generic function, that perform an AJAX request
+    ajaxRequest("/sale", function(data)
+    {
+        //first clean
+        $('#saleList').empty();
+
+        //Iteration
+        data.forEach(function(sale)
+        {
+            $('#saleList').append('<tr>' +
+                '<td>' + sale.id + '</td>' +
+                '<td>' + sale.saleFirstName + '</td>' +
+                '<td>' + sale.saleAddress + '</td>' +
+                '<td>' + sale.saleItem + '</td>' +
+                '<td>' + sale.saleQtyItem + '</td>' +
+                '<td>' + sale.salePrice.toFixed(2) + '</td>' +
+                '<td>' + sale.saleDate + '</td>' +
+                '<td>' + sale.saleTotalPrice.toFixed(2) + '</td>' +
+                '<td><button class="btn btn-primary" onclick="openEditModal(' + sale.id + ')">Edit</button></td>' +
+                '</tr>'
+            )
+        });
+    });
+}
 
 
