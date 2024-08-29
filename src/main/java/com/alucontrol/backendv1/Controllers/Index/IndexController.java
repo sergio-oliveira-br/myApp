902bdeb998cdb4a578559af3ae1336902cc68c12
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-08-29 17:58:54 +0100

    Update IndexController.java
    cleaning

--- src/main/java/com/alucontrol/backendv1/Controllers/Index/IndexController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Index/IndexController.java
@@ -31,39 +31,4 @@ public class IndexController
         this.indexService =  indexService;
         this.rentRepository = rentRepository;
     }
-
-//    /** Main endpoint for the home page, indicating that the endpoint is active  */
-//    @GetMapping("/")
-//    public String home()
-//    {
-//        return "index";
-//    }
-
-
-    /** Endpoint to get back the number of rent UNPAID from DB
-     *  Pointing to indexScript.js*/
-//    @GetMapping("/qtyRentUnpaid")
-//    public Long getQtyRentUnpaid()
-//    {
-//        return indexService.getQtyRentUnpaid();
-//    }
-
-//
-//    /** Endpoint to get the number of rent witch has the status "NEW"
-//     *  Pointing to indexScript.js */
-//    @GetMapping("/qtyRentStatusNew")
-//    public ResponseEntity<Long> getQtyRentStatusNew()
-//    {
-//        Long qtyRentStatusNew = indexService.getQtyRentStatusNew();
-//        return ResponseEntity.ok(qtyRentStatusNew);
-//    }
-//
-//    /** Endpoint to get the number of rent witch has the status "IN PROGRESS"
-//     *  Pointing to indexScript.js */
-//    @GetMapping("/qtyRentStatusInProgress")
-//    public ResponseEntity<Long> getQtyRentStatusInProgress()
-//    {
-//       Long qtyRentStatusInProgress = indexService.getQtyRentStatusInProgress();
-//       return ResponseEntity.ok(qtyRentStatusInProgress);
-//    }
 }
