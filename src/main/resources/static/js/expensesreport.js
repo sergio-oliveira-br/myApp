74751ac0de291fe97f4f5eab86dcdbf7f522a106
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-08-31 13:11:03 +0100

    Update expensesreport.js
    disabling

--- src/main/resources/static/js/expensesreport.js
+++ src/main/resources/static/js/expensesreport.js
@@ -1,110 +1,110 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie
- * Project Commencing May 2024
- * Version: 1.0
- */
-
-/** Load content when the page is loaded */
-$(document).ready(function ()
-{
-    //load the table with all expenses
-    getAllExpenses();
-
-
-});
-
-
-/**
- Page: Expense Report
- Item: Table
- Method: The script will load all expenses in a table
-    by clicking on filter by category
- */
-function getExpensesByCategory(category)
-{
-    console.log("Category selected: " + category);
-
-    //Build the URL
-    let url = "/expensesByCategory?expenseCategory=" + category;
-
-    //Call the Ajax request
-    ajaxRequest(url, function (data)
-    {
-        //clean the table
-        $('#expensesListReport').empty();
-
-        //Iteration
-        data.forEach(function (expense) {
-            $('#expensesListReport').append('<tr>' +
-                '<td>' + expense.id + '</td>' +
-                '<td>' + expense.expenseDescription + '</td>' +
-                '<td>' + expense.expenseAmount + '</td>' +
-                '<td>' + expense.expenseDate + '</td>' +
-                '<td>' + expense.expenseCategory + '</td>' +
-                '<td>' + expense.expenseAdditionalNotes + '</td>' +
-                '</tr>'
-            );
-        })
-    });
-}
-
-
-/**
- Page: Expense Report
- Item: Table
- Method: The script will load all expenses in a table by clicking on without filter by ALL
- */
-function getAllExpenses()
-{
-    //Call the Ajax Request
-    ajaxRequest("/expense", function (data)
-    {
-        //clean the table
-        $('#expensesListReport').empty();
-
-        //Iteration
-        data.forEach(function (expense) {
-            $('#expensesListReport').append('<tr>' +
-                '<td>' + expense.id + '</td>' +
-                '<td>' + expense.expenseDescription + '</td>' +
-                '<td>' + expense.expenseAmount + '</td>' +
-                '<td>' + expense.expenseDate + '</td>' +
-                '<td>' + expense.expenseCategory + '</td>' +
-                '<td>' + expense.expenseAdditionalNotes + '</td>' +
-            '</tr>');
-        });
-    });
-}
-
-/**
- Page: Expense Report
- Item: Table -> Date (Month and Year) in NAVBAR
- Method: This will filter date by selecting Month and Year
- */
-function getExpensesByMonth(year, month) {
-
-    //Building the URL
-    let url = "/expensesByDate?year=" + encodeURIComponent(year) + "&month=" + encodeURIComponent(month);
-
-    //Call the ajax request
-    ajaxRequest(url,function (data)
-    {
-        $('#expensesListReport').empty();
-        data.forEach(function (expense) {
-            $('#expensesListReport').append('<tr>' +
-                '<td>' + expense.id + '</td>' +
-                '<td>' + expense.expenseDescription + '</td>' +
-                '<td>' + expense.expenseAmount + '</td>' +
-                '<td>' + expense.expenseDate + '</td>' +
-                '<td>' + expense.expenseCategory + '</td>' +
-                '<td>' + expense.expenseAdditionalNotes + '</td>' +
-                '</tr>'
-            );
-        });
-    });
-}
+// /**
+//  * National College of Ireland - NCI
+//  *    Higher Diploma in Computing
+//  *         Final Project
+//  *              ---
+//  * Author: Sergio Vinicio da Silva Oliveira
+//  * ID: x23170981@student.ncirl.ie
+//  * Project Commencing May 2024
+//  * Version: 1.0
+//  */
+//
+// /** Load content when the page is loaded */
+// $(document).ready(function ()
+// {
+//     //load the table with all expenses
+//     getAllExpenses();
+//
+//
+// });
+//
+//
+// /**
+//  Page: Expense Report
+//  Item: Table
+//  Method: The script will load all expenses in a table
+//     by clicking on filter by category
+//  */
+// function getExpensesByCategory(category)
+// {
+//     console.log("Category selected: " + category);
+//
+//     //Build the URL
+//     let url = "/expensesByCategory?expenseCategory=" + category;
+//
+//     //Call the Ajax request
+//     ajaxRequest(url, function (data)
+//     {
+//         //clean the table
+//         $('#expensesListReport').empty();
+//
+//         //Iteration
+//         data.forEach(function (expense) {
+//             $('#expensesListReport').append('<tr>' +
+//                 '<td>' + expense.id + '</td>' +
+//                 '<td>' + expense.expenseDescription + '</td>' +
+//                 '<td>' + expense.expenseAmount + '</td>' +
+//                 '<td>' + expense.expenseDate + '</td>' +
+//                 '<td>' + expense.expenseCategory + '</td>' +
+//                 '<td>' + expense.expenseAdditionalNotes + '</td>' +
+//                 '</tr>'
+//             );
+//         })
+//     });
+// }
+//
+//
+// /**
+//  Page: Expense Report
+//  Item: Table
+//  Method: The script will load all expenses in a table by clicking on without filter by ALL
+//  */
+// function getAllExpenses()
+// {
+//     //Call the Ajax Request
+//     ajaxRequest("/expense", function (data)
+//     {
+//         //clean the table
+//         $('#expensesListReport').empty();
+//
+//         //Iteration
+//         data.forEach(function (expense) {
+//             $('#expensesListReport').append('<tr>' +
+//                 '<td>' + expense.id + '</td>' +
+//                 '<td>' + expense.expenseDescription + '</td>' +
+//                 '<td>' + expense.expenseAmount + '</td>' +
+//                 '<td>' + expense.expenseDate + '</td>' +
+//                 '<td>' + expense.expenseCategory + '</td>' +
+//                 '<td>' + expense.expenseAdditionalNotes + '</td>' +
+//             '</tr>');
+//         });
+//     });
+// }
+//
+// /**
+//  Page: Expense Report
+//  Item: Table -> Date (Month and Year) in NAVBAR
+//  Method: This will filter date by selecting Month and Year
+//  */
+// function getExpensesByMonth(year, month) {
+//
+//     //Building the URL
+//     let url = "/expensesByDate?year=" + encodeURIComponent(year) + "&month=" + encodeURIComponent(month);
+//
+//     //Call the ajax request
+//     ajaxRequest(url,function (data)
+//     {
+//         $('#expensesListReport').empty();
+//         data.forEach(function (expense) {
+//             $('#expensesListReport').append('<tr>' +
+//                 '<td>' + expense.id + '</td>' +
+//                 '<td>' + expense.expenseDescription + '</td>' +
+//                 '<td>' + expense.expenseAmount + '</td>' +
+//                 '<td>' + expense.expenseDate + '</td>' +
+//                 '<td>' + expense.expenseCategory + '</td>' +
+//                 '<td>' + expense.expenseAdditionalNotes + '</td>' +
+//                 '</tr>'
+//             );
+//         });
+//     });
+// }
