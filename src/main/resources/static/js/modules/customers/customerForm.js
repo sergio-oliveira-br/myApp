35a3107133a5a7876d0e51f5fb642bb633b30fc3
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-08-31 11:54:32 +0100

    new structure - customers

---
+++ src/main/resources/static/js/modules/customers/customerForm.js
@@ -0,0 +1,45 @@
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
+ * Project Commencing May 2024 | Version: 1.0
+ * Refactoring & New Features - Aug 2024 | Version: 2.0
+ */
+
+//This file handles the submission of the client form and updating the client table.
+//src/main/resources/static/js/modules/customers/customerForm.js
+
+// customerForm.js
+
+/**
+ * Method: Send the customer data by using AJAX
+ */
+formSubmission('#customerForm', '/saveCustomer', customerFormData, customerSaveSuccess, saveError);
+
+/**
+ * Get the form data from the form
+ */
+function customerFormData() {
+    return {
+        firstName: $('#customerFirstName').val(),
+        lastName: $('#customerLastName').val(),
+        phoneNumber: $('#customerPhoneNumber').val(),
+        additionalInfo: $('#additionalInformation').val(),
+        city: $('#customerCity').val()
+    };
+}
+
+/**
+ * Callback function for success
+ */
+function customerSaveSuccess(response) {
+    alert('Cliente salvo com sucesso!');
+    console.log(response);
+    loadCustomers(); // Update the customer table
+
+    // Clear the form by resetting it
+    document.getElementById('customerForm').reset();
+}
---
+++ src/main/resources/static/js/modules/customers/customerTable.js
@@ -0,0 +1,33 @@
+/**
+ * National College of Ireland - NCI
+ *    Higher Diploma in Computing
+ *         Final Project
+ *              ---
+ * Author: Sergio Vinicio da Silva Oliveira
+ * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
+ * Project Commencing May 2024 | Version: 1.0
+ * Refactoring & New Features - Aug 2024 | Version: 2.0
+ */
+
+//This file is responsible for loading and rendering the client table.
+//src/main/resources/static/js/modules/customers/customerTable.js
+
+/**
+ * Page: Customers
+ * Item: Table
+ * Method: Create a table with all customers via AJAX
+ */
+function loadCustomers() {
+    ajaxRequest("/customers", function (data) {
+        $('#customerList').empty(); // First clean
+        data.forEach(function (customer) {
+            $('#customerList').append('<tr>' +
+                '<td>' + customer.id + '</td>' +
+                '<td>' + customer.firstName + '</td>' +
+                '<td>' + customer.lastName + '</td>' +
+                '<td>' + customer.city + '</td>' +
+                '<td>' + customer.additionalInfo + '</td>' +
+                '</tr>');
+        });
+    });
+}
