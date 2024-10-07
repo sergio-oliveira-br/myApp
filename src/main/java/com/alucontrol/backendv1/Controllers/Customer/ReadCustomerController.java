7975ce59438c3db2d7c9aa0ba175e41078fe3822
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-07 21:15:52 +0100

    Create ReadCustomerController.java
    Act:
    -> This class was refactored from CustomersReadController to ReadCustomerController
    
    this verb Read is better to explain what the main idea of the class

---
+++ src/main/java/com/alucontrol/backendv1/Controllers/Customer/ReadCustomerController.java
@@ -0,0 +1,28 @@
+package com.alucontrol.backendv1.Controllers.Customer;
+
+import com.alucontrol.backendv1.Model.Customer;
+import com.alucontrol.backendv1.Service.Customer.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class ReadCustomerController {
+    public final CustomerService customerService;
+
+    public ReadCustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    //endpoint para obter todos os clientes presente na base de dados
+    @GetMapping
+    public ResponseEntity<List<Customer>> getCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    //endpoint para obter um cliente expecifico atraves do ID
+}
