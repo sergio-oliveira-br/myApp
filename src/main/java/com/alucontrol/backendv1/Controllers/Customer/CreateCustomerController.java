b6979a6a3b9b48544990a5ac9377c276ada7454b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-07 20:42:54 +0100

    Create CreateCustomerController.java
    Act:
    -> this class had the name refactored,
    
    -> this approach fetch the clean code methodology, here the controller will be responsible only for call services functions

---
+++ src/main/java/com/alucontrol/backendv1/Controllers/Customer/CreateCustomerController.java
@@ -0,0 +1,23 @@
+package com.alucontrol.backendv1.Controllers.Customer;
+
+import com.alucontrol.backendv1.Model.Customer;
+import com.alucontrol.backendv1.Service.Customer.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CreateCustomerController {
+
+    private final CustomerService customerService;
+
+    public CreateCustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    //This endpoint is responsible to create a new Customer into a DB
+    @PostMapping("/api/v1/createCustomer")
+    public ResponseEntity<Customer> createCustomer(@Validated @RequestBody Customer customer) {
+        return customerService.saveCustomer(customer);
+    }
+}
