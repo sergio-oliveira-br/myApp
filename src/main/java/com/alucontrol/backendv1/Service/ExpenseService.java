48fa3a346ff65dd3eecc45c054291c285fad7b0c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-08 09:32:05 +0100

    Update ExpenseService.java
    Act:
    -> The business logic will be concentrated into class service
    Here is the saveExpense, which will use to create new expense

--- src/main/java/com/alucontrol/backendv1/Service/ExpenseService.java
+++ src/main/java/com/alucontrol/backendv1/Service/ExpenseService.java
@@ -1,29 +1,24 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie
- * Project Commencing May 2024
- * Version: 1.0
- */
 package com.alucontrol.backendv1.Service;
 
+import com.alucontrol.backendv1.Model.Expense;
 import com.alucontrol.backendv1.Repository.ExpenseRepository;
+import com.alucontrol.backendv1.Util.LoggerUtil;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-/** This Service Class has methods that contain business logic.*/
 @Service
-public class ExpenseService
-{
-    //Repository for access to Expense data
+public class ExpenseService {
+
     private final ExpenseRepository expenseRepository;
 
-    //Constructor responsible for injecting the repository
-    public ExpenseService(ExpenseRepository expenseRepository)
-    {
+    public ExpenseService(ExpenseRepository expenseRepository) {
         this.expenseRepository = expenseRepository;
     }
-    
+
+    //Method para Salvar novas Despesas
+    public ResponseEntity<Expense> saveExpense(Expense expense) {
+        Expense savedExpense = expenseRepository.save(expense);
+        LoggerUtil.info("Expense saved successfully: " + savedExpense.toString());
+        return ResponseEntity.ok(savedExpense);
+    }
 }
