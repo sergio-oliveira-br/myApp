43cfff79d06f1117986d20ebfee34b23e01c8765
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-08 09:33:56 +0100

    Update CreateExpenseController.java
    Act:
    -> approach changed. this method will just call  the method responsible to do something, in this case save s new expense, then this method will create the new data

--- src/main/java/com/alucontrol/backendv1/Controllers/Expense/CreateExpenseController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Expense/CreateExpenseController.java
@@ -1,68 +1,52 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie
- * Project Commencing May 2024
- * Version: 1.0
- */
 package com.alucontrol.backendv1.Controllers.Expense;
 
 
 import com.alucontrol.backendv1.Model.Expense;
 import com.alucontrol.backendv1.Repository.ExpenseRepository;
+import com.alucontrol.backendv1.Service.ExpenseService;
 import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-/** This controller is dedicated to endpoints that create and update records
- * It is the responsibility of this layer to receive requests, call methods from the service layer, and return HTTP responses */
+
 @RestController
-public class CreateExpenseController
-{
-    private final ExpenseRepository expenseRepository;
+@RequestMapping("/api/v1/create-expense")
+public class CreateExpenseController {
 
-    //Constructor responsible for injecting the repository
-    private CreateExpenseController(ExpenseRepository expenseRepository) {
-        this.expenseRepository = expenseRepository;
+    private final ExpenseService expenseService;
+
+    public CreateExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
     }
 
-    /** Endpoint to sent Expenses to DB */
-    @PostMapping("/saveExpense")
-    public ResponseEntity<Expense> saveExpense(@RequestBody Expense expense) {
-
-        //Log
-        LoggerUtil.info("Starting to save Expense with data: " + expense.toString());
-
-        //Save the customer in the database
-        Expense savedExpense = expenseRepository.save(expense);
-
-        //Log and Return a response HTTP 200 - OK - saving the expense
-        LoggerUtil.info("Expense Saved Successfully: " + savedExpense.toString());
-        return ResponseEntity.ok(savedExpense);
+    @PostMapping()
+    public ResponseEntity<Expense> createExpense(@RequestBody Expense expense) {
+        return expenseService.saveExpense(expense);
     }
 
-    /** Endpoint to update a specific expense by ID */
-    @PutMapping("/expense/{id}")
-    public ResponseEntity<Expense> updateExpense(@RequestBody Expense expenseUpdated, @PathVariable Long id) {
 
-        Optional<Expense> expenseOptional = expenseRepository.findById(id);
 
-        if (expenseOptional.isPresent()) {
-            LoggerUtil.info("Starting to update Expense with data: " + expenseOptional.toString());
 
-            Expense savedExpense = expenseRepository.save(expenseUpdated);
-
-            LoggerUtil.info("Expense Updated Successfully: " + savedExpense.toString());
-            return ResponseEntity.ok(savedExpense);
-        }
-        else {
-            LoggerUtil.info("Updating Expense with id " + id + " failed.");
-            return ResponseEntity.notFound().build();
-        }
-    }
+//
+//    /** Endpoint to update a specific expense by ID */
+//    @PutMapping("/expense/{id}")
+//    public ResponseEntity<Expense> updateExpense(@RequestBody Expense expenseUpdated, @PathVariable Long id) {
+//
+//        Optional<Expense> expenseOptional = expenseRepository.findById(id);
+//
+//        if (expenseOptional.isPresent()) {
+//            LoggerUtil.info("Starting to update Expense with data: " + expenseOptional.toString());
+//
+//            Expense savedExpense = expenseRepository.save(expenseUpdated);
+//
+//            LoggerUtil.info("Expense Updated Successfully: " + savedExpense.toString());
+//            return ResponseEntity.ok(savedExpense);
+//        }
+//        else {
+//            LoggerUtil.info("Updating Expense with id " + id + " failed.");
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
 }
