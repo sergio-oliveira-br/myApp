492fdb52eca0fdf6ce4a540664a3e23158f8f24a
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-09 13:21:05 +0100

    Update UpdateProductController.java
    Act:
    -> class create to handle product update service

--- src/main/java/com/alucontrol/backendv1/Controllers/Product/UpdateProductController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Product/UpdateProductController.java
@@ -1,4 +1,22 @@
 package com.alucontrol.backendv1.Controllers.Product;
 
+import com.alucontrol.backendv1.Model.Product;
+import com.alucontrol.backendv1.Service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/update-product")
 public class UpdateProductController {
+
+    private final ProductService productService;
+
+    public UpdateProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+        return productService.saveProductChanges(product, id);
+    }
 }
