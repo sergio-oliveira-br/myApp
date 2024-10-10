1cb616a8f19ef4bad4c5948201c869d44d089ee3
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-10 10:31:32 +0100

    Update CreateRentController.java
    Act:
    -> name of the class has been refactored. This class will be responsible only for creations

--- src/main/java/com/alucontrol/backendv1/Controllers/Rent/CreateRentController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Rent/CreateRentController.java
@@ -1,13 +1,3 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie
- * Project Commencing May 2024
- * Version: 1.0
- */
 package com.alucontrol.backendv1.Controllers.Rent;
 
 import com.alucontrol.backendv1.Util.LoggerUtil;
@@ -19,25 +9,19 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-/**This controller is dedicated to endpoints that create and update records
- * It is the responsibility of this layer to receive requests, call methods from the service layer, and return HTTP responses.*/
 @RestController
-public class RentCreateUpdateController
-{
-    //Repository for access to product data
-    private final RentRepository rentRepository;
+@RequestMapping("/api/v1/rent/create-rent")
+public class CreateRentController {
 
+    private final RentRepository rentRepository;
     private final StockService stockService;
 
-    //Constructor responsible for injecting the repository
-    public RentCreateUpdateController(RentRepository rentRepository, StockService stockService)
-    {
+    public CreateRentController(RentRepository rentRepository, StockService stockService) {
         this.rentRepository = rentRepository;
         this.stockService = stockService;
     }
 
-    /** Endpoint to send rent */
-    @PostMapping("/saveRent")
+
     public ResponseEntity<Rent> saveRent( @RequestBody Rent rent){
 
         //Log
