dfbada4482336c4b1ede4e3cd7de9b174575fb35
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-10 11:01:38 +0100

    Update CreateRentController.java
    Act:
    -> method removed - this is allocated into service class.
    
    -> the class create is responsible only for creations

--- src/main/java/com/alucontrol/backendv1/Controllers/Rent/CreateRentController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Rent/CreateRentController.java
@@ -30,7 +30,6 @@ public class CreateRentController {
     }
 
 
-
     /** Endpoint to get a specific rent by ID (by clicking on Edit into the table)*/
     @GetMapping("/rent/{id}")
     public ResponseEntity<Rent> getRentById(@PathVariable Long id) {
@@ -38,53 +37,6 @@ public class CreateRentController {
         if (rentOptional.isPresent()) {
             return ResponseEntity.ok(rentOptional.get());
         } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-
-    /** Endpoint to update a specific rent by ID */
-    @PutMapping("/rent/{id}")
-    public ResponseEntity<Rent> updateRent(@PathVariable Long id, @RequestBody Rent updatedRent) {
-        LoggerUtil.info("Starting to update rent with data: " + updatedRent.toString());
-
-        Optional<Rent> rentOptional = rentRepository.findById(id);
-        if (rentOptional.isPresent()) {
-
-            Rent rent = rentOptional.get();
-
-            rent.setRentFirstName(updatedRent.getRentFirstName());
-            //rent.setRentLastName(updatedRent.getRentLastName());
-            rent.setRentAddress(updatedRent.getRentAddress());
-            rent.setRentItem(updatedRent.getRentItem());
-            rent.setRentPrice(updatedRent.getRentPrice());
-            rent.setRentQtyItem(updatedRent.getRentQtyItem());
-            rent.setRentStarts(updatedRent.getRentStarts());
-            rent.setRentEnds(updatedRent.getRentEnds());
-            rent.setRentTotalDays(updatedRent.getRentTotalDays());
-            rent.setRentTotalPrice(updatedRent.getRentTotalPrice());
-            rent.setRentDetails(updatedRent.getRentDetails());
-            rent.setRentPaymentStatus(updatedRent.getRentPaymentStatus());
-            rent.setRentStatus(updatedRent.getRentStatus());
-
-            if(updatedRent.getRentStatus().equals("Novo")) {
-                //Create a log
-                LoggerUtil.info("Status: Novo -> Nenhuma alteraçao no estoque foi realizada!");
-            }
-
-            else if (updatedRent.getRentStatus().equals("Em andamento")) {
-                 //If the status changed from new to in progress, then the stock have to decrease
-                stockService.subtractStock(updatedRent.getRentItem(), updatedRent.getRentQtyItem());
-            }
-            else if (updatedRent.getRentStatus().equals("Encerrado")) {
-                //Execute the method to return the qyt to the stock
-                stockService.addStockByRentalStatusFinished(updatedRent.getRentItem(), updatedRent.getRentQtyItem());
-            }
-            Rent savedRent = rentRepository.save(rent);
-            LoggerUtil.info("Rent updated: " + savedRent.toString());
-            return ResponseEntity.ok(savedRent);
-        }
-        else {
             return ResponseEntity.notFound().build();
         }
     }
