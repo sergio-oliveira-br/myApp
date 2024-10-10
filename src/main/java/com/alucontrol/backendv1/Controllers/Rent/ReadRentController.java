115e9f35cf2bb3c53856f51d462dc230fcf3510e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-10 11:33:39 +0100

    Update ReadRentController.java
    Act:
    -> method get all rents added
    
    -> method get a unique rent by id added

--- src/main/java/com/alucontrol/backendv1/Controllers/Rent/ReadRentController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Rent/ReadRentController.java
@@ -1,13 +1,3 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie
- * Project Commencing May 2024
- * Version: 1.0
- */
 package com.alucontrol.backendv1.Controllers.Rent;
 
 import com.alucontrol.backendv1.Exception.ResourceNotFoundException;
@@ -15,41 +5,47 @@ import com.alucontrol.backendv1.Model.Rent;
 import com.alucontrol.backendv1.Repository.Rent.RentPaymentStatusRepository;
 import com.alucontrol.backendv1.Repository.Rent.RentStatusRepository;
 import com.alucontrol.backendv1.Repository.RentRepository;
+import com.alucontrol.backendv1.Service.RentServices;
 import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/** This controller is dedicated to endpoints that read data */
 @RestController
-public class ReadRentController
-{
+@RequestMapping("/api/v1/rent")
+public class ReadRentController {
+
     //Repository for access to product data
     private final RentRepository rentRepository;
     private final RentStatusRepository rentStatusRepository;
     private final RentPaymentStatusRepository rentPaymentStatusRepository;
+    private final RentServices rentServices;
 
     //Constructor responsible for injecting the repository
-    public ReadRentController(RentRepository rentRepository, RentStatusRepository rentStatusRepository, RentPaymentStatusRepository rentPaymentStatusRepository)
-    {
+    public ReadRentController(RentServices rentServices, RentRepository rentRepository, RentStatusRepository rentStatusRepository, RentPaymentStatusRepository rentPaymentStatusRepository) {
         this.rentRepository = rentRepository;
         this.rentStatusRepository = rentStatusRepository; //focus on queries related to the status of the Rent
         this.rentPaymentStatusRepository = rentPaymentStatusRepository;
+        this.rentServices = rentServices;
     }
 
-    /** Endpoint to get back all rent */
-    @GetMapping("/rent")
-    public ResponseEntity<List<Rent>> getAllRent()
-    {
-        List<Rent> rent = rentRepository.findAll(); //using the method findALL from CRUD
-        LoggerUtil.info("Fetched " + rent.size() + " rent records"); //Create a log
 
-        //Returning the list of all rent records in a ResponseEntity with status OK
-        return ResponseEntity.ok(rent);
+    @GetMapping
+    public ResponseEntity<List<Rent>> getAllRent() {
+        return rentServices.findAllRents();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Rent> getRentById(@PathVariable Long id) {
+        return rentServices.findRentById(id);
+    }
+
+
+
+
+
+
 
     /** Endpoint to retrieve Rent by selecting the "Month" and "Year" in expenseDate field */
     @GetMapping("/rentByDate")
