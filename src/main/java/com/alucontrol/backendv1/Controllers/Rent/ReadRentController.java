ddaff67f9d3c301422bd1892cee9379802c96ee7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-10 11:46:33 +0100

    Update ReadRentController.java
    Act:
    -> method removed and allocated into service class
    
    -> method invoked from service class

--- src/main/java/com/alucontrol/backendv1/Controllers/Rent/ReadRentController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Rent/ReadRentController.java
@@ -30,121 +30,112 @@ public class ReadRentController {
         return rentServices.findRentById(id);
     }
 
-
-
-
-
-
-
-    /** Endpoint to retrieve Rent by selecting the "Month" and "Year" in expenseDate field */
-    @GetMapping("/rentByDate")
-    public ResponseEntity<List<Rent>> getRentByDate(String year, String month)
-    {
-        List<Rent> rents = rentRepository.findByYearAndMonth(year, month);
-
-        if (rents.isEmpty()) {
-            LoggerUtil.error("No Rent found for year " + year + " and month " + month);
-            throw new ResourceNotFoundException("No Rent found");
-        }
-        return ResponseEntity.ok(rents);
-    }
-
-    /** Endpoint to retrieve Rent by selecting the Rent Payment Status */
-    @GetMapping("/rentByPaymentStatus")
-    public ResponseEntity<List<Rent>> getRentByPaymentStatus(String paymentStatus)
-    {
-        List<Rent> rents = rentRepository.findRentByPaymentStatus(paymentStatus);
-
-        if (rents.isEmpty()) {
-            LoggerUtil.error("No Rent found for payment status " + paymentStatus);
-            throw new ResourceNotFoundException("No Rent found");
-        }
-        return ResponseEntity.ok(rents);
-    }
-
-    /** Endpoint to retrieve Rent by selecting the Rent Payment Status */
-    @GetMapping("/rentByStatus")
-    public ResponseEntity<List<Rent>> getRentByStatus(String rentStatus)
-    {
-        List<Rent> rents = rentRepository.findRentByStatus(rentStatus);
-
-        if (rents.isEmpty()) {
-            LoggerUtil.error("No Rent found for status " + rentStatus);
-            throw new ResourceNotFoundException("No Rent found");
-        }
-        return ResponseEntity.ok(rents);
-    }
-
-    /** Endpoint to retrieve Rent by searching the customer name */
-    @GetMapping("/rentByName")
-    public ResponseEntity<List<Rent>> getRentByName(String name)
-    {
-        List<Rent> rents = rentRepository.findRentByFirstName(name);
-
-        if (rents.isEmpty()) {
-            LoggerUtil.error("No Rent found for name: " + name);
-            throw new ResourceNotFoundException("No Rent found");
-        }
-        return ResponseEntity.ok(rents);
-    }
-
-    /** Endpoint to get the number (qty) of rent by selecting the status
-     * This method is pointing to indexScript.js */
-    @GetMapping("/qtyRentByStatus")
-    public ResponseEntity<Long> getQtyRentByStatus(@RequestParam String rentStatus) {
-
-        LoggerUtil.info("Fetching Rent Status: " + rentStatus);
-
-        Long countRentByStatus;
-
-        try {
-            countRentByStatus = rentStatusRepository.countRentByStatus(rentStatus);
-
-            if(countRentByStatus == null){
-                LoggerUtil.error("No Rent found for status " + rentStatus);
-
-                throw new ResourceNotFoundException("No Rent found for status " + rentStatus);
-            }
-
-        }catch (Exception e){
-            LoggerUtil.error("An error occurred while fetching the quantity of rent by status." +
-                   "Error: " + e.getMessage(), e);
-
-            throw new ResourceNotFoundException("An error occurred while fetching the quantity of rent by status. |" +
-                    "Rent Status: " + rentStatus +
-                    " | Error: " + e.getMessage() + e);
-        }
-
-        return ResponseEntity.ok(countRentByStatus);
+    @GetMapping("/rent-by-date")
+    public ResponseEntity<List<Rent>> getRentByDate(String year, String month) {
+        return rentServices.findRentByDate(year, month);
     }
 
 
-
-    /** Endpoint to get the number (qty) of rent by selecting the payment status
-     * This method is pointing to indexScript.js */
-    @GetMapping("/qtyRentByPaymentStatus")
-    public ResponseEntity<Long> getQtyRentByPaymentStatus(@RequestParam String paymentStatus) {
-        LoggerUtil.info("Fetching Rent Payment Status: " + paymentStatus);
-        Long countRentByPaymentStatus;
-
-        try {
-            countRentByPaymentStatus = rentPaymentStatusRepository.countRentByPaymentStatus(paymentStatus);
-
-            if(countRentByPaymentStatus == null){
-                LoggerUtil.error("No Rent found for payment status: " + paymentStatus);
-                throw new ResourceNotFoundException("No Rent found for payment status: " + paymentStatus);
-
-            }
-        }catch (Exception e){
-            LoggerUtil.error("An error occurred while fetching the quantity of rent by payment status. | " +
-                    "Error: " + e.getMessage(), e);
-
-            throw new ResourceNotFoundException("An error occurred while fetching the quantity of rent by payment status. |" +
-                    "Rent Payment Status: " + paymentStatus +
-                    " | Error: " + e.getMessage() + e);
-        }
-
-        LoggerUtil.debug("Rent Payment Status: " + paymentStatus);
-        return ResponseEntity.ok(countRentByPaymentStatus);
-    }
+//
+//
+//
+//
+//    /** Endpoint to retrieve Rent by selecting the Rent Payment Status */
+//    @GetMapping("/rentByPaymentStatus")
+//    public ResponseEntity<List<Rent>> getRentByPaymentStatus(String paymentStatus)
+//    {
+//        List<Rent> rents = rentRepository.findRentByPaymentStatus(paymentStatus);
+//
+//        if (rents.isEmpty()) {
+//            LoggerUtil.error("No Rent found for payment status " + paymentStatus);
+//            throw new ResourceNotFoundException("No Rent found");
+//        }
+//        return ResponseEntity.ok(rents);
+//    }
+//
+//    /** Endpoint to retrieve Rent by selecting the Rent Payment Status */
+//    @GetMapping("/rentByStatus")
+//    public ResponseEntity<List<Rent>> getRentByStatus(String rentStatus)
+//    {
+//        List<Rent> rents = rentRepository.findRentByStatus(rentStatus);
+//
+//        if (rents.isEmpty()) {
+//            LoggerUtil.error("No Rent found for status " + rentStatus);
+//            throw new ResourceNotFoundException("No Rent found");
+//        }
+//        return ResponseEntity.ok(rents);
+//    }
+//
+//    /** Endpoint to retrieve Rent by searching the customer name */
+//    @GetMapping("/rentByName")
+//    public ResponseEntity<List<Rent>> getRentByName(String name)
+//    {
+//        List<Rent> rents = rentRepository.findRentByFirstName(name);
+//
+//        if (rents.isEmpty()) {
+//            LoggerUtil.error("No Rent found for name: " + name);
+//            throw new ResourceNotFoundException("No Rent found");
+//        }
+//        return ResponseEntity.ok(rents);
+//    }
+//
+//    /** Endpoint to get the number (qty) of rent by selecting the status
+//     * This method is pointing to indexScript.js */
+//    @GetMapping("/qtyRentByStatus")
+//    public ResponseEntity<Long> getQtyRentByStatus(@RequestParam String rentStatus) {
+//
+//        LoggerUtil.info("Fetching Rent Status: " + rentStatus);
+//
+//        Long countRentByStatus;
+//
+//        try {
+//            countRentByStatus = rentStatusRepository.countRentByStatus(rentStatus);
+//
+//            if(countRentByStatus == null){
+//                LoggerUtil.error("No Rent found for status " + rentStatus);
+//
+//                throw new ResourceNotFoundException("No Rent found for status " + rentStatus);
+//            }
+//
+//        }catch (Exception e){
+//            LoggerUtil.error("An error occurred while fetching the quantity of rent by status." +
+//                   "Error: " + e.getMessage(), e);
+//
+//            throw new ResourceNotFoundException("An error occurred while fetching the quantity of rent by status. |" +
+//                    "Rent Status: " + rentStatus +
+//                    " | Error: " + e.getMessage() + e);
+//        }
+//
+//        return ResponseEntity.ok(countRentByStatus);
+//    }
+//
+//
+//
+//    /** Endpoint to get the number (qty) of rent by selecting the payment status
+//     * This method is pointing to indexScript.js */
+//    @GetMapping("/qtyRentByPaymentStatus")
+//    public ResponseEntity<Long> getQtyRentByPaymentStatus(@RequestParam String paymentStatus) {
+//        LoggerUtil.info("Fetching Rent Payment Status: " + paymentStatus);
+//        Long countRentByPaymentStatus;
+//
+//        try {
+//            countRentByPaymentStatus = rentPaymentStatusRepository.countRentByPaymentStatus(paymentStatus);
+//
+//            if(countRentByPaymentStatus == null){
+//                LoggerUtil.error("No Rent found for payment status: " + paymentStatus);
+//                throw new ResourceNotFoundException("No Rent found for payment status: " + paymentStatus);
+//
+//            }
+//        }catch (Exception e){
+//            LoggerUtil.error("An error occurred while fetching the quantity of rent by payment status. | " +
+//                    "Error: " + e.getMessage(), e);
+//
+//            throw new ResourceNotFoundException("An error occurred while fetching the quantity of rent by payment status. |" +
+//                    "Rent Payment Status: " + paymentStatus +
+//                    " | Error: " + e.getMessage() + e);
+//        }
+//
+//        LoggerUtil.debug("Rent Payment Status: " + paymentStatus);
+//        return ResponseEntity.ok(countRentByPaymentStatus);
+//    }
 }
