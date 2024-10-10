abbffafce0816ddd749f7eaf4f9f8eee889a580e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-10 11:39:31 +0100

    Update ReadRentController.java

--- src/main/java/com/alucontrol/backendv1/Controllers/Rent/ReadRentController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Rent/ReadRentController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ReadRentController {
 
     private final RentServices rentServices;
-    
+
     public ReadRentController(RentServices rentServices) {
         this.rentServices = rentServices;
     }
