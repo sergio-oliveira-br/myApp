63aa07163dca9d8d437dd939c39529efeb1d8337
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-10 11:39:25 +0100

    Update CreateRentController.java
    cleaning

--- src/main/java/com/alucontrol/backendv1/Controllers/Rent/CreateRentController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Rent/CreateRentController.java
@@ -1,26 +1,17 @@
 package com.alucontrol.backendv1.Controllers.Rent;
 
 import com.alucontrol.backendv1.Service.RentServices;
-import com.alucontrol.backendv1.Util.LoggerUtil;
 import com.alucontrol.backendv1.Model.Rent;
-import com.alucontrol.backendv1.Repository.RentRepository;
-import com.alucontrol.backendv1.Service.StockService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/rent/create-rent")
 public class CreateRentController {
 
-    private final RentRepository rentRepository;
-    private final StockService stockService;
     private final RentServices rentServices;
 
-    public CreateRentController(RentRepository rentRepository, StockService stockService, RentServices rentServices) {
-        this.rentRepository = rentRepository;
-        this.stockService = stockService;
+    public CreateRentController(RentServices rentServices) {
         this.rentServices = rentServices;
     }
 
@@ -28,7 +19,4 @@ public class CreateRentController {
     public ResponseEntity<Rent> createRent (@RequestBody Rent rent) {
         return rentServices.saveRent(rent);
     }
-
-
-
 }
