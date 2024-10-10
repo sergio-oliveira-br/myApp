674bf8d34f0d2c56ae8c3ce95f33c68c42e51e4e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-10 11:02:24 +0100

    Update RentServices.java
    Act:
    -> this method has been moved from the controller class. This is responsible to update a rent

--- src/main/java/com/alucontrol/backendv1/Service/RentServices.java
+++ src/main/java/com/alucontrol/backendv1/Service/RentServices.java
@@ -1,11 +1,14 @@
 package com.alucontrol.backendv1.Service;
 
+import com.alucontrol.backendv1.Exception.ResourceNotFoundException;
 import com.alucontrol.backendv1.Model.Rent;
 import com.alucontrol.backendv1.Repository.RentRepository;
 import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RentServices {
@@ -40,6 +43,42 @@ public class RentServices {
         LoggerUtil.info("Rent saved successfully: " + savedRent.toString());
 
         return ResponseEntity.ok(savedRent);
+    }
+
+    //Metodo de Atualização de algueis que ja existentem na DB buscando o ID.
+    public ResponseEntity<Rent> saveRentChanges(Rent updatedRent, Long id) {
+
+        Optional<Rent> rentOptional = rentRepository.findById(id);
+
+        if(rentOptional.isPresent()) {
+
+            Rent rent = rentOptional.get();
+
+            String rentStatus = rent.getRentStatus();
+            String product = rent.getRentItem();
+            int productQty = rent.getRentQtyItem();
+
+            //Verifica o status do rent para substração ou adição do estoque
+            if(rentStatus.equals("Em andamento")) {
+                stockService.subtractStock(product, productQty);
+                LoggerUtil.info("Rent saved successfully: " + updatedRent.toString());
+
+                return ResponseEntity.ok(updatedRent);
+            }
+
+            else if(rentStatus.equals("Encerrado")) {
+                stockService.addStockByRentalStatusFinished(product, productQty);
+                LoggerUtil.info("Rent saved successfully: " + updatedRent.toString());
+
+                return ResponseEntity.ok(updatedRent);
+            }
+
+            //Este considereda que o status é "novo"
+            LoggerUtil.info("Rent saved successfully: " + updatedRent.toString());
+            return ResponseEntity.ok(updatedRent);
+        }
+
+      throw new ResourceNotFoundException("Rent with id" + id + " not found");
     }
 
 
