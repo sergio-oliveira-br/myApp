2d756270ab9c0f0376ee35070c4ef87248db719e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-11 10:38:58 +0100

    Create StockManagerService.java
    Act:
    -> this class has been created to allocate the business logic about stock management
    
    -> method to decrease and check availability created

---
+++ src/main/java/com/alucontrol/backendv1/Service/Inventory/StockManagerService.java
@@ -0,0 +1,52 @@
+package com.alucontrol.backendv1.Service.Inventory;
+
+import com.alucontrol.backendv1.Exception.ResourceNotFoundException;
+import com.alucontrol.backendv1.Model.Product;
+import com.alucontrol.backendv1.Repository.ProductRepository;
+import com.alucontrol.backendv1.Util.LoggerUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class StockManagerService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public StockManagerService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //Este metodo é responsavel por realizar a subtração do item quando um aluguel for realizado
+    public void decreaseStockAfterRental (String itemDescription, int requestedQuantity) {
+
+        Optional<Product> productOptional = productRepository.findByItemDescription(itemDescription);
+
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+
+            if(isStockSufficient(itemDescription, requestedQuantity)) {
+                product.setItemAvailableQty(product.getItemAvailableQty() - requestedQuantity);
+
+                productRepository.save(product);
+                return;
+            }
+
+            throw new ResourceNotFoundException("O produto '" + itemDescription + "' não possui a quantidade " +   requestedQuantity + " solicitada em estoque. " +
+                    "Atualmente a quantidade disponivel é " + product.getItemAvailableQty());
+        }
+
+        throw new ResourceNotFoundException("O produto '" + itemDescription + "' nao existe.");
+    }
+
+    //Verifica se a quantiade solicitada é maior que a quantidade disponivel
+    public boolean isStockSufficient (String itemDescription, int requestedQuantity) {
+
+        Optional<Product> productOptional = productRepository.findByItemDescription(itemDescription);
+        Product product = productOptional.get();
+
+        return product.getItemAvailableQty() >= requestedQuantity;
+    }
+}
