a655b07ea77acf190b58b4c65db387dc57a4620d
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-13 18:19:58 +0100

    Create UpdateSaleController.java
    Act:
    -> Method implemented. This is responsible to save sales changes

--- src/main/java/com/alucontrol/backendv1/Service/SaleService.java
+++ src/main/java/com/alucontrol/backendv1/Service/SaleService.java
@@ -1,10 +1,13 @@
 package com.alucontrol.backendv1.Service;
 
+import com.alucontrol.backendv1.Exception.ResourceNotFoundException;
 import com.alucontrol.backendv1.Model.Sale;
 import com.alucontrol.backendv1.Repository.SaleRepository;
 import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SaleService {
@@ -21,5 +24,19 @@ public class SaleService {
         Sale savedSale = saleRepository.save(sale);
         LoggerUtil.info("Venda salva com sucesso. ID: " + savedSale.toString());
         return ResponseEntity.ok(savedSale);
+    }
+
+    //Metodo para atualizar uma venda ja existente no BD atraves do ID.
+    public ResponseEntity<Sale> saveSaleChanges(Sale sale, Long id) {
+
+       Optional<Sale> optionalSale = saleRepository.findById(id);
+       if (optionalSale.isPresent()) {
+           Sale savedSale = saleRepository.save(sale);
+
+           LoggerUtil.info("Venda salva com sucesso. ID: " + savedSale.toString());
+           return ResponseEntity.ok(savedSale);
+       }
+
+       throw new ResourceNotFoundException("Venda ID: " + id + " não encontrada.");
     }
 }
