20edc41577533ddec5453de32b053f752d01369c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-13 18:40:50 +0100

    Update ReadSaleController.java
    Act:
    -> cleaning
    -> method to get sale by ID is invoking the service class called sales

--- src/main/java/com/alucontrol/backendv1/Controllers/Sale/ReadSaleController.java
+++ src/main/java/com/alucontrol/backendv1/Controllers/Sale/ReadSaleController.java
@@ -1,15 +1,11 @@
 package com.alucontrol.backendv1.Controllers.Sale;
 
 import com.alucontrol.backendv1.Model.Sale;
-import com.alucontrol.backendv1.Repository.ProductRepository;
-import com.alucontrol.backendv1.Repository.SaleRepository;
 import com.alucontrol.backendv1.Service.SaleService;
-import com.alucontrol.backendv1.Util.LoggerUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/sales")
@@ -27,16 +23,8 @@ public class ReadSaleController {
         return saleService.findAllSales();
     }
 
-    /** Endpoint to get a specific SALE by ID (by clicking on Edit into the table)*/
-    @GetMapping("/sale/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Sale> getSaleById(@PathVariable Long id) {
-        LoggerUtil.info("Looking for sale record with id: " + id);
-
-        Optional<Sale> sale = saleRepository.findById(id);
-        if (sale.isPresent()) {
-            LoggerUtil.info("Fetched sale record with id: " + id);
-            return ResponseEntity.ok(sale.get());
-        }
-        return ResponseEntity.notFound().build();
+        return saleService.findSaleById(id);
     }
 }
