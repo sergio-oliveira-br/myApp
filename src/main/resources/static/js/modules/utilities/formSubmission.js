243b6515bbe86136155826c33ebab560f698dc4a
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-15 22:57:26 +0100

    Update formSubmission.js
    Act:
    -> method to validate the form included
    
    -> instead build a new ajax request, I am using the ajaxRequest form types created into ajaxUtils
    this helps clean code with dont repeat your self

--- src/main/resources/static/js/modules/utilities/formSubmission.js
+++ src/main/resources/static/js/modules/utilities/formSubmission.js
@@ -1,51 +1,18 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//Method to send data from forms via AJAX
 //src/main/resources/static/js/modules/utilities/formSubmission.js
 
-/**
- Page: Customers, Rent, Sales, Product and Expenses
- Item: Form
- Method: Send the customer data by using AJAX
- */
-function formSubmission(formId, url, formDataFunction, successCallback)
-{
-    $(document).ready(function() {
-        let currentDate = new Date();
+function formSubmission(formId, url, formDataFunction, successCallback) {
 
-        //Get the form ID that contain the all data. Remember: FormID is ID from the form on HTML
-        $(formId).on('submit', function(event)
-        {
-            //Prevents the default behavior of the browser, which would reload the page and send the form data synchronously
+    $(document).ready(function() {
+        $(formId).on('submit', function(event) {
             event.preventDefault();
 
-            //Get the form data
+            // Obtenha o elemento do formulário
+            const formElement = this; // 'this' refere-se ao formulário que disparou o evento
             let formData = formDataFunction();
 
-            $.ajax({
-                url: url,
-                data: JSON.stringify(formData),
-                type: 'POST',
-                contentType: 'application/json',
-                success: function(response){
-                    if(successCallback) {
-                        successCallback(response);
-                        console.log(formData, currentDate);
-                    }
-                },
-                error: function(xhr, status, error) {
-                    errorHandler(xhr);
-                }
-            });
+            if(validateForm (formElement)) {
+                ajaxRequestTypePost(url, formData, successCallback);
+            }
         });
     });
 }
