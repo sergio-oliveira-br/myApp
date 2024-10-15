c3d93147972e6e2561e159fc2ebc705d59d2e3e7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-15 22:54:52 +0100

    Update formValidation.js
    Act:
    -> make the form validation useful

--- src/main/resources/static/js/modules/utilities/formValidation.js
+++ src/main/resources/static/js/modules/utilities/formValidation.js
@@ -1,31 +1,39 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file contain code related to form validation using Bootstrap.
 //src/main/resources/static/js/modules/utilities/formValidation.js
 
-
 // Reference: https://getbootstrap.com/docs/5.0/forms/validation/
-// Checking form submissions if there are invalid fields
-(function () {
-    var forms = document.querySelectorAll('.needs-validation');
 
-    Array.prototype.slice.call(forms).forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
+// Checking form submissions if there are invalid fields
+function validateForm(form) {
+
+    if (form instanceof HTMLFormElement && form.checkValidity()) {
+        return true; // Form is valid
+    } else {
+        const firstInvalidElement = $(form).find(':invalid').first();
+        if (firstInvalidElement) {
+            firstInvalidElement.focus();
+        }
+        return false;
+    }
+}
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict';
+    const forms = document.querySelectorAll('.needs-validation');
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            // Aqui estamos usando validateForm passando o elemento 'form'
+            if (!validateForm(form)) {
                 event.preventDefault();
                 event.stopPropagation();
-                console.log('Checking the form...');
             }
+
             form.classList.add('was-validated');
         }, false);
     });
 })();
+
+
