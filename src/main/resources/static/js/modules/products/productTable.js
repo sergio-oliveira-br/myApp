9f03299f3ff9faf453043578fb1eb2718a1fb888
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-16 12:33:45 +0100

    Update productTable.js
    cleaning

--- src/main/resources/static/js/modules/products/productTable.js
+++ src/main/resources/static/js/modules/products/productTable.js
@@ -1,26 +1,8 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file will contain functions that deal with the creation and updating of the product table on the page
 //src/main/resources/static/js/modules/products/productTable.js
 
-/**
- * Page: Products
- * Info: Creates a table with all products via AJAX
- */
 function loadProductTable() {
     ajaxRequestTypeGet("/api/v1/product", function(data) {
         $('#productList').empty();
-
-        console.log("There are " + data.length + " products in your DB");
 
         //Iteration
         data.forEach(function(product) {
@@ -37,19 +19,16 @@ function loadProductTable() {
     });
 }
 
-/**
- * Update
- * Method: Populate product select dropdown in the edit modal form
- */
-function updateLoadProductForm() {
-    console.log("Here where the method updateLoadProductForm() is used. !IMPORTANT: CHECK THE NEED");
 
-    ajaxRequestTypeGet("/product", function(data) {
-        let productSelect = $('#editItemQty');
-        productSelect.empty();
-        data.forEach(function(product) {
-            productSelect.append('<option value="' + product.id + '">' + product.name + '</option>');
-        });
-    });
-}
+// function updateLoadProductForm() {
+//     console.log("Here where the method updateLoadProductForm() is used. !IMPORTANT: CHECK THE NEED");
+//
+//     ajaxRequestTypeGet("/product", function(data) {
+//         let productSelect = $('#editItemQty');
+//         productSelect.empty();
+//         data.forEach(function(product) {
+//             productSelect.append('<option value="' + product.id + '">' + product.name + '</option>');
+//         });
+//     });
+// }
 
