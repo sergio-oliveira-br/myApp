60842f148db34887b6734917e4ce36a0d8e0167f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-16 10:46:26 +0100

    Update ajaxUtils.js
    cleaning

--- src/main/resources/static/js/modules/utilities/ajaxUtils.js
+++ src/main/resources/static/js/modules/utilities/ajaxUtils.js
@@ -1,21 +1,6 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file contain generic AJAX request functions.
 //src/main/resources/static/js/modules/utilities/ajaxUtils.js
 
-/**
- * Method: ajaxRequest(url, successCallback)
- * Info: Generic function to perform an AJAX request and handle success and error responses
- */
+
 function ajaxRequest(url, successCallback) {
     $.ajax({
         url: url,
@@ -64,6 +49,7 @@ function ajaxRequestTypePost(url, formData, successCallback) {
         type: "POST", //Used to submit new data to a server.
         contentType: 'application/json',
         data: JSON.stringify(formData),
+
         success: function(response){
             if(successCallback) {
                 successCallback(response);
