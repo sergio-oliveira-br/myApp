7807b4b4a4ad4cc75b29a86208a19bbff1fcbcaa
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-16 10:46:12 +0100

    Update customerForm.js
    cleaning

--- src/main/resources/static/js/modules/customers/customerForm.js
+++ src/main/resources/static/js/modules/customers/customerForm.js
@@ -1,25 +1,8 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file handles the submission of the client form and updating the client table.
 //src/main/resources/static/js/modules/customers/customerForm.js
 
-/**
- * Method: Send the customer data by using AJAX
- */
 submitForm('#customerForm', '/api/v1/customer/create-customer', customerFormData, customerSaveSuccess);
 
-/**
- * Get the form data from the form
- */
+
 function customerFormData() {
     return {
         firstName: $('#customerFirstName').val(),
@@ -30,13 +13,8 @@ function customerFormData() {
     };
 }
 
-/**
- * Callback function for success
- */
+
 function customerSaveSuccess(response) {
     alert('Cliente salvo com sucesso!');
-    loadCustomersTable(); // Update the customer table
-
-    // Clear the form by resetting it
-    document.getElementById('customerForm').reset();
+    loadCustomersTable();
 }
