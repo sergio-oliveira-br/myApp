12795edcd674bc22ca7f59ad7d4808905c7fa9bd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-17 16:18:01 +0100

    Update productUtils.js
    cleaning

--- src/main/resources/static/js/modules/products/productUtils.js
+++ src/main/resources/static/js/modules/products/productUtils.js
@@ -1,21 +1,6 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file contain product-related utility functions.
 //src/main/resources/static/js/modules/products/productUtils.js
 
-/**
- * Method: loadProductListByProductType(productType)
- * Info: Create a list of all items and display it in the Product field of the Sales and Rent Form
- */
+
 function loadProductListByProductType(productType) {
     ajaxRequestTypeGet("/api/v1/product/product-by-type?productType=" + encodeURIComponent(productType), function(data) {
 
@@ -26,7 +11,8 @@ function loadProductListByProductType(productType) {
         productSelects.forEach(function (selectElement) {
             selectElement.empty();
             data.forEach(function (product) {
-                selectElement.append('<option id="' + product.id + '" price="' + product.itemPrice + '">' + product.itemDescription + '</option>');
+                selectElement.append('<option id="' + product.id + '" price="' + product.itemPrice + '">'
+                    + product.itemDescription + '</option>');
             });
         });
     });
@@ -50,7 +36,6 @@ $('#rentItem, #editRentItem, #saleItem, #editSaleItem').change(function() {
 
     else if ($(this).attr('id') === 'saleItem') {
         $('#salePrice').val(price);
-        //loadTotalPriceSales(); // Update the total price for sales
     }
 
     else if ($(this).attr('id') === 'editSaleItem') {
