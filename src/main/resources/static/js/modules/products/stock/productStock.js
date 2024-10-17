6a7991f121650e08693d916c5e6359f91f628908
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-17 16:16:29 +0100

    Update productStock.js
    cleaning

--- src/main/resources/static/js/modules/products/stock/productStock.js
+++ src/main/resources/static/js/modules/products/stock/productStock.js
@@ -1,20 +1,5 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//Methods related to stock of products.
 //src/main/resources/static/js/modules/products/stock/productStock.js
 
-/**
- * Method: Create a list of all items and display it in the Product field of the Sales form
- */
 function loadCurrentStockByType(productType) {
     let stockModal = new bootstrap.Modal(document.getElementById('stockModal'));
     stockModal.show();
