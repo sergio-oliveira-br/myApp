079c1bbcbcc02e9dd1a77f2d105c1c4ad25c8741
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 11:14:57 +0100

    Update expensesModal.js
    Act:
    -> approach changed, instead repeat the method, now the function requestPUT is called to handle the PUT method
    
    -> cleaning

--- src/main/resources/static/js/modules/expenses/expensesModal.js
+++ src/main/resources/static/js/modules/expenses/expensesModal.js
@@ -1,27 +1,8 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//Methods related to the modal of editing expenses.
 //src/main/resources/static/js/modules/expenses/expensesModal.js
 
-/**
- * Page: Expense
- * Item: Form (modal) -> OpenEditModal() -> SubmitEditForm()
- * Method: This is a jQuery method that allows the user to make asynchronous requests to the server
- * to send or receive data without having to reload the page
- */
 function openExpenseEditModal(expenseId) {
-    ajaxRequestTypeGet('/api/v1/expense/' + expenseId, function (expense) {
-        console.log(expense);
 
+    ajaxRequestTypeGet('/api/v1/expense/' + expenseId, function (expense) {
         $('#editExpenseId').val(expenseId);
         $('#editExpenseDescription').val(expense.expenseDescription);
         $('#editExpenseAmount').val(expense.expenseAmount);
@@ -35,13 +16,8 @@ function openExpenseEditModal(expenseId) {
     });
 }
 
-/**
- * Page: Expense
- * Item: Form (modal) -> Submit
- * Method: Submit the form with updated expense data
- */
+
 function submitExpenseEditForm() {
-    let currentDate = new Date();
 
     let itemData = {
         id: $('#editExpenseId').val(),
@@ -52,35 +28,11 @@ function submitExpenseEditForm() {
         expenseAdditionalNotes: $('#editExpenseAdditionalNotes').val()
     };
 
-    console.log(itemData);
-
-    $.ajax({
-        url: '/api/v1/expense/update-expense/' + itemData.id,
-        type: 'PUT',
-        contentType: 'application/json',
-        data: JSON.stringify(itemData),
-
-        success: function (response) {
-            let successMsg = "Despesa alterada com sucesso";
-
-            alert(successMsg);
-            console.log(successMsg, currentDate);
-
-            $('#editModal').modal('hide');//close the modal
-
-            loadExpenseTable();//Reload the table
-        },
-
-        error: function (xhr, status, error) {
-            try {
-                let response = JSON.parse(xhr.responseText);
-                alert('Oops! Ocorreu um erro. ' + response);
-            }
-            catch (parseError) {
-                console.log('Análise do erro: ', parseError);
-            }
-        }
-    });
+    ajaxRequestTypePut('/api/v1/expense/update-expense/' + itemData.id, itemData, function (response) {
+        alert('Despesa alterada com sucesso!');
+        $('#editModal').modal('hide');
+        loadExpenseTable();
+    })
 }
 
 //Confirmation message for form submission
@@ -97,6 +49,5 @@ document.getElementById('editModal').addEventListener('submit', function (event)
         //Build and print a message
         let cancelMsg = 'Operaçao cancelada pelo usuario! \nOs dados não foram alterados.';
         alert(cancelMsg)
-        console.log(cancelMsg);
     }
 })
