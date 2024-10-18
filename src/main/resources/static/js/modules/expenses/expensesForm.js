453983bbf338c96ae91cf46f0e0ecfe19faae37c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 11:12:54 +0100

    Update expensesForm.js
    cleaning

--- src/main/resources/static/js/modules/expenses/expensesForm.js
+++ src/main/resources/static/js/modules/expenses/expensesForm.js
@@ -1,26 +1,7 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//Methods related to sending the expense form.
 //src/main/resources/static/js/modules/expenses/expensesForm.js
 
-/**
- * Item: Form
- * Method: Send the expense data by using AJAX
- */
 submitForm('#expenseForm', '/api/v1/expense/create-expense', expenseFormData, expenseSaveSuccess);
 
-/**
- * Get the form data from the form
- */
 function expenseFormData() {
     return {
         expenseDescription: $('#expenseDescription').val(),
@@ -31,14 +12,7 @@ function expenseFormData() {
     };
 }
 
-/**
- * Item: Form, Save
- * Method: Callback function for success
- */
 function expenseSaveSuccess(response) {
     alert('Despesa adicionada com sucesso!');
     loadExpenseTable(); // update the expense table
-
-    // Clear the form by resetting it
-    document.getElementById('expenseForm').reset();
 }
