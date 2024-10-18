2c759f3bb30faa2ab9248b01b6850f3dd8562ca6
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 17:11:51 +0100

    Update salesModal.js
    Act:
    -> approach changed, instead build a Ajax request, now its called the method PUT

--- src/main/resources/static/js/modules/sales/salesModal.js
+++ src/main/resources/static/js/modules/sales/salesModal.js
@@ -1,26 +1,7 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file is responsible for handling the sales editing modal.
 //src/main/resources/static/js/modules/sales/salesModal.js
 
-/**
- * Page: Sales
- * Item: Form (modal)
- * Method: This is a jQuery method that allows the user to make asynchronous requests to the server
- * to send or receive data without having to reload the page.
- */
 
 function populateSaleModal(saleId) {
-    console.log("Open Edit Modal, ID: " + saleId);
 
     ajaxRequestTypeGet('/api/v1/sale/' + saleId, function (sale){
         $('#editSaleId').val(sale.id);
@@ -39,14 +20,8 @@ function populateSaleModal(saleId) {
     });
 }
 
-/**
- Page: Sales
- Item: Form (modal)
- Method: Send the data to update my database
- */
 
 function saleFormSubmissionModal(){
-    let currentDate = new Date();
 
     let saleData = {
         id: $('#editSaleId').val(),
@@ -61,27 +36,10 @@ function saleFormSubmissionModal(){
         saleDetails: $('#editSaleDetails').val()
     };
 
-    //this is a log to check what's it will send
-    console.log(saleData);
-    $.ajax({
-        url: '/api/v1/sale/update-sale/' + saleData.id,
-        type: 'PUT',
-        contentType: 'application/json',
-        data: JSON.stringify(saleData),
+    ajaxRequestTypePut('/api/v1/sale/update-sale/' + saleData.id, saleData, function (response) {
 
-        success: function (response) {
-            let successMsg = "Venda alterada com sucesso";
-            alert(successMsg);
-
-            $('#editModal').modal('hide');
-
-            console.log(successMsg, currentDate);
-
-            loadSalesTable();
-        },
-        error: function (xhr, status, error) {
-            //call the function from errorHandling.js file
-            errorHandler(error);
-        }
-    });
+        alert('Venda alterado com sucesso!');
+        $('#editModal').modal('hide');
+        loadSalesTable();
+    })
 }
