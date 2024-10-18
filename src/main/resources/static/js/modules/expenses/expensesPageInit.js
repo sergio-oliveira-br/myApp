a1565f3d11e26088e9a8d892ca2edccfaee793af
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 11:13:02 +0100

    Update expensesPageInit.js
    cleaning

--- src/main/resources/static/js/modules/expenses/expensesPageInit.js
+++ src/main/resources/static/js/modules/expenses/expensesPageInit.js
@@ -1,15 +1,3 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file centralizes all boot calls in one place, making it easy to understand and maintain.
 //src/main/resources/static/js/modules/expenses/expensesPageInit.js
 
 $(document).ready(function () {
@@ -18,8 +6,6 @@ $(document).ready(function () {
 
     //(Modal) Send the data by clicking on submit button
     $('#editModal').on('submit', function(e) {
-        //The preventDefault() method cancels the event if it is cancelable,
-        //meaning that the default action that belongs to the event will not occur.
         e.preventDefault();
     });
 })
