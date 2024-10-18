17caba4cc1a72117eb11b4e1dda53c24e8ae3393
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 17:10:11 +0100

    Update salesTable.js
    cleaning

--- src/main/resources/static/js/modules/sales/salesTable.js
+++ src/main/resources/static/js/modules/sales/salesTable.js
@@ -1,25 +1,11 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-
-//This file is responsible for loading and rendering the sales table.
 //src/main/resources/static/js/modules/sales/salesTable.js
 
-/**
- * Page: Sale - Table
- * Method: Load all sales items into a table
- */
 function loadSalesTable() {
+
     ajaxRequestTypeGet("/api/v1/sale", function (data) {
+
         $('#saleList').empty();
+
         data.forEach(function (sale) {
             $('#saleList').append('<tr>' +
                 '<td>' + sale.id + '</td>' +
