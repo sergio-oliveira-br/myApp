92064af110de7f87e862073097a2666d41960137
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 21:44:18 +0100

    Update indexPageInit.js
    cleaning

--- src/main/resources/static/js/modules/index/indexPageInit.js
+++ src/main/resources/static/js/modules/index/indexPageInit.js
@@ -1,19 +1,6 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file centralizes all boot calls in one place, making it easy to understand and maintain.
 //src/main/resources/static/js/modules/index/indexPageInit.js
 
 $(document).ready(function () {
-    console.log('Index Page Init');
     //This will display a number by status within the cards
     loadQtyRentByStatus('Novo');
     loadQtyRentByStatus('Em andamento');
