0620a95078bd342e74b061ddf503b8ce4717c997
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 18:18:28 +0100

    Update rentModal.js
    act:
    -> cleaning
    
    -> approach changed, instead build a request, now  its used the ajaxRequestPUT method to handle updates
    
    -> feedback/alert changed, after updated

--- src/main/resources/static/js/modules/rent/rentModal.js
+++ src/main/resources/static/js/modules/rent/rentModal.js
@@ -1,22 +1,5 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file is responsible for handling the rental editing modal.
 //src/main/resources/static/js/modules/rent/rentModal.js
 
-/**
- * Page: Rent
- * Item: Form (modal)
- * Method: Calculates the difference of the days
- */
 function updateRentDays() {
     let newStart = $('#editRentStarts').val();
     let newEnd = $('#editRentEnds').val();
@@ -30,11 +13,7 @@ function updateRentDays() {
 document.getElementById('editRentStarts').addEventListener('change',updateRentDays);
 document.getElementById('editRentEnds').addEventListener('change',updateRentDays);
 
-/**
- * Page: Rent
- * Item: Form (modal)
- * Method: Calculates the total price on Modal
- */
+
 function updateTotalPrice() {
     let newRentPriceModal = parseFloat($('#editRentPrice').val().replace(',', '.')) || 0;
     let newRentQtyItemModal = parseInt($('#editRentQtyItem').val()) || 0;
@@ -45,14 +24,8 @@ function updateTotalPrice() {
     $('#editRentTotalPrice').val(newRentTotalPriceModal.toFixed(2));
 }
 
-/**
- * Page: Rent and Index
- * Item: Form (modal)
- * Method: This is a jQuery method that allows the user to make asynchronous requests to the server
- * to send or receive data without having to reload the page.
- */
+
 function populateRentModal(rentId) {
-    console.log("Open Edit Modal, ID: " + rentId);
 
     ajaxRequestTypeGet("/api/v1/rent/" + rentId, function(rent) {
         $('#editRentId').val(rent.id);
@@ -75,13 +48,8 @@ function populateRentModal(rentId) {
     });
 }
 
-/**
- Page: Rent and Index
- Item: Form (modal)
- Method: Send the data to update my database
- */
+
 function rentFormSubmissionModal() {
-    let currentDate = new Date();
 
     let rentData = {
         id: $('#editRentId').val(),
@@ -100,28 +68,10 @@ function rentFormSubmissionModal() {
         rentStatus: $('#editRentStatus').val()
     };
 
-    //this is a log to check what's it will send
-    console.log(rentData);
+    ajaxRequestTypePut('/api/v1/rent/update-rent/' + rentData.id, rentData, function(response) {
 
-    $.ajax({ //allows updating parts of a web page without reloading the entire page
-        url: '/api/v1/rent/update-rent/' + rentData.id, //indicates the endpoint
-        type: 'PUT', //HTTP request methods used to INSERT data to the server (backend), indicating by the endpoint specified by the URL
-        contentType: 'application/json',
-        data: JSON.stringify(rentData),
-
-        success: function (response) {
-            let successMsg = "Aluguel alterado com sucesso";
-            alert(successMsg);
-
-            $('#editModal').modal('hide');
-
-            console.log(successMsg, currentDate);
-
-            loadRentTable();
-        },
-        error: function(xhr, status, error) {
-            //call the function from errorHandling.js file
-            errorHandler(error);
-        }
+       alert("Aluguel 'ID: " +rentData.id +  ", "  +  rentData.rentFirstName + "' alterado com sucesso!");
+       $('#editModal').modal('hide');
+       loadSalesTable();
     });
 }
