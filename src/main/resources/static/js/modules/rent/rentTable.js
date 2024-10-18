9f9d3ece162627b25a528c3bebe067ced66f4b5e
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 18:16:11 +0100

    Update rentTable.js
    Act:
    -> cleaning
    
    -> two col disabled (address and un price)

--- src/main/resources/static/js/modules/rent/rentTable.js
+++ src/main/resources/static/js/modules/rent/rentTable.js
@@ -1,22 +1,5 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file is responsible for loading and rendering the rent table.
 //src/main/resources/static/js/modules/rent/rentTable.js
 
-/**
- * Page: Rent
- * Item: Table
- * Method: The script will load all items in a table
- */
 function loadRentTable() {
     ajaxRequestTypeGet("/api/v1/rent", function(data) {
         $('#rentList').empty(); // First clean
@@ -24,10 +7,9 @@ function loadRentTable() {
             $('#rentList').append('<tr>' +
                 '<td>' + rent.id + '</td>' +
                 '<td>' + rent.rentFirstName + '</td>' +
-                '<td>' + rent.rentAddress + '</td>' +
                 '<td>' + rent.rentItem + '</td>' +
                 '<td>' + rent.rentQtyItem + '</td>' +
-                '<td>' + rent.rentPrice.toFixed(2) + '</td>' + // Formatting to two decimal places
+                // '<td>' + rent.rentPrice.toFixed(2) + '</td>' + // Formatting to two decimal places
                 '<td>' + rent.rentStarts + '</td>' +
                 '<td>' + rent.rentEnds + '</td>' +
                 '<td>' + rent.rentTotalDays + '</td>' +
