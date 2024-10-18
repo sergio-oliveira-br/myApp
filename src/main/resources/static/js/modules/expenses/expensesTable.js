ef40996761eb09f45dd9df60510f0b090bd07609
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-18 11:13:10 +0100

    Update expensesTable.js
    cleaning

--- src/main/resources/static/js/modules/expenses/expensesTable.js
+++ src/main/resources/static/js/modules/expenses/expensesTable.js
@@ -1,27 +1,10 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//Method to load the table of expenses.
 //src/main/resources/static/js/modules/expenses/expensesTable.js
 
-/**
- * Page: Expense
- * Item: Table
- * Method: The script will load all expenses in a table
- */
 function loadExpenseTable() {
-    ajaxRequestTypeGet("/api/v1/expense", function (data) {
-        $('#expenseTable').empty();
 
-        console.log('There are ' + data.length + ' expenses in your expenses DB.');
+    ajaxRequestTypeGet("/api/v1/expense", function (data) {
+
+        $('#expenseTable').empty();
 
         data.forEach(function (expense) {
             $('#expenseTable').append('<tr>' +
