e0c48eb6f9a1a6ac095cdfa36ba23a001841aee5
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-20 10:18:02 +0100

    Update rentTableReport.js
    cleaning

--- src/main/resources/static/js/modules/rent/rentReport/rentTableReport.js
+++ src/main/resources/static/js/modules/rent/rentReport/rentTableReport.js
@@ -1,20 +1,5 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file is responsible for loading and rendering the rent table on REPORT page.
 //src/main/resources/static/js/modules/rent/rentReport/rentTableReport.js
 
-/**
- * Function to render rent table on report page
- */
 function renderRentTableReport(data) {
     //clean
     $('#rentList').empty();
