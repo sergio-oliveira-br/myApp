82e7298bbd7ea4c412427742b56f8cdb9df246b4
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-20 10:19:51 +0100

    Update rentPaymentStatusFilter.js
    cleaning

--- src/main/resources/static/js/modules/rent/rentReport/rentPaymentStatusFilter.js
+++ src/main/resources/static/js/modules/rent/rentReport/rentPaymentStatusFilter.js
@@ -1,22 +1,5 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//Methods to filter rentals by payment status.
 //src/main/resources/static/js/modules/rent/rentReport/rentPaymentStatusFilter.js
 
-/**
- * Page: Rent
- * Item: Table
- * Method: Filter rents by payment status
- */
 function getRentByPaymentStatus(status) {
     let url = "/api/v1/rent/rent-by-payment-status?paymentStatus=" + encodeURIComponent(status);
     ajaxRequestTypeGet(url, renderRentTableReport);
