e653c300c6cf9462b605aee5d149f4253b60a2bf
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-20 10:19:39 +0100

    Update rentSearchFilter.js
    cleaning

--- src/main/resources/static/js/modules/rent/rentReport/rentSearchFilter.js
+++ src/main/resources/static/js/modules/rent/rentReport/rentSearchFilter.js
@@ -1,22 +1,5 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//Methods to search rentals by name or date.
 //src/main/resources/static/js/modules/rent/rentReport/rentSearchFilter.js
 
-/**
- * Page: Rent
- * Item: Search and Display
- * Method: Search rents by name or date.
- */
 function searchRent() {
     let year = $('#yearInput').val();
     let month = $('#monthInput').val();
@@ -29,28 +12,22 @@ function searchRent() {
         } else {
             getRentByName(name + '%'); // Finds any values that start with "name"
         }
+
     } else if (year && month) {
         getRentByMonth(year, month);
+
     } else {
         alert("Desculpe! Para pesquisar, por favor entre com o Nome ou a combinação de Mês e Ano.");
     }
 }
 
-/**
- * Page: Rent Report
- * Item: Table -> Date (Month and Year) in NAVBAR
- * Method: Filter rents by selecting month and year
- */
+
 function getRentByMonth(year, month) {
     let url = "/api/v1/rent/rent-by-date?year=" + encodeURIComponent(year) + "&month=" + encodeURIComponent(month);
     ajaxRequestTypeGet(url, renderRentTableReport);
 }
 
-/**
- * Page: Rent Report
- * Item: Table -> Search (Name) in NAVBAR
- * Method: Filter rents by name
- */
+
 function getRentByName(name) {
     let url = "/api/v1/rent/rent-by-name?customerName=" + encodeURIComponent(name);
     ajaxRequestTypeGet(url, renderRentTableReport);
