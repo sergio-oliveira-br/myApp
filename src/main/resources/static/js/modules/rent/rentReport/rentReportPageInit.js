45defddca6d88a3194a0b709a8fd297753c93a31
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-20 10:19:45 +0100

    Update rentReportPageInit.js
    cleaning

--- src/main/resources/static/js/modules/rent/rentReport/rentReportPageInit.js
+++ src/main/resources/static/js/modules/rent/rentReport/rentReportPageInit.js
@@ -1,15 +1,3 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file centralizes all boot calls in one place, making it easy to understand and maintain.
 //src/main/resources/static/js/modules/rent/rentReport/rentReportPageInit.js
 
 $(document).ready(function () {
