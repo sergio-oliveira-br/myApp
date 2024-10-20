092f59c65a627f990ad53860cdb1593595fee98b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-20 10:13:02 +0100

    Update rentGetAllData.js
    issue:
    # URL was out of data
    
    Act:
    -> cleaning
    
    -> URL updated

--- src/main/resources/static/js/modules/rent/rentReport/rentGetAllData.js
+++ src/main/resources/static/js/modules/rent/rentReport/rentGetAllData.js
@@ -1,22 +1,5 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie, oliveira-sergio@outlook.com
- * Project Commencing May 2024 | Version: 1.0
- * Refactoring & New Features - Aug 2024 | Version: 2.0
- */
-
-//This file is responsible for loading and rendering the rent table on RENT REPORT PAGE.
 //src/main/resources/static/js/modules/rent/rentReport/rentGetAllData.js
 
-/**
- * Page: Rent Report
- * Item: Table
- * Method: Load all rent records
- */
 function getAllRent() {
-    ajaxRequestTypeGet("/rent", renderRentTableReport);
+    ajaxRequestTypeGet("/api/v1/rent", renderRentTableReport);
 }
