d898d5b2f548378ed3794378b67feceb33be535f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-28 19:26:39 +0000

    Update ResourceNotFoundException.java
    cleaning

--- src/main/java/com/alucontrol/backendv1/Exception/ResourceNotFoundException.java
+++ src/main/java/com/alucontrol/backendv1/Exception/ResourceNotFoundException.java
@@ -1,20 +1,8 @@
-/**
- * National College of Ireland - NCI
- *    Higher Diploma in Computing
- *         Final Project
- *              ---
- * Author: Sergio Vinicio da Silva Oliveira
- * ID: x23170981@student.ncirl.ie
- * Project Commencing May 2024
- * Version: 1.0
- */
 package com.alucontrol.backendv1.Exception;
 
-/** This class represents a custom exception that can be thrown when a resource is not found */
-public class ResourceNotFoundException extends RuntimeException
-{
-    public ResourceNotFoundException(String message)
-    {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
