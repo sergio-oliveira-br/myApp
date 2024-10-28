b8cb347f491b91d25068ea1478cb0efbba0084ad
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-10-28 19:27:02 +0000

    Create DataAccessException.java
    Class extended

---
+++ src/main/java/com/alucontrol/backendv1/Exception/DataAccessException.java
@@ -0,0 +1,8 @@
+package com.alucontrol.backendv1.Exception;
+
+public class DataAccessException extends RuntimeException {
+
+    public DataAccessException(String message) {
+        super(message);
+    }
+}
