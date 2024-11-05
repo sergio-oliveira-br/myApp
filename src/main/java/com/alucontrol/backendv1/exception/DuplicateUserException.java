23ad47b45c051cb83c38fb9051df87048aaaecf6
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-05 21:31:20 +0000

    Update DuplicateUserException.java
    Act:
    Throwable removed

--- src/main/java/com/alucontrol/backendv1/exception/DuplicateUserException.java
+++ src/main/java/com/alucontrol/backendv1/exception/DuplicateUserException.java
@@ -2,9 +2,7 @@ package com.alucontrol.backendv1.exception;
 
 public class DuplicateUserException extends RuntimeException {
 
-    // Adicionar um construtor com causa (Throwable):
-    // Possibilita capturar a exceção original (se houver alguma outra exceção interna).
-    public DuplicateUserException(String message, Throwable cause) {
+    public DuplicateUserException(String message) {
         super(message);
     }
 }
