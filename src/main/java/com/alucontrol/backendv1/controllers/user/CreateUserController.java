e0f925e67126bd96de286e6a7d024a75819b5165
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-07 12:33:33 +0000

    Update CreateUserController.java
    Issue:
    #To make possible deal with direct request from HTML, without using script, the controller need to receive a Parameters using @RequestParam.
    
    Act:
    -> RequestBody replaced to Request Param approach

--- src/main/java/com/alucontrol/backendv1/controllers/user/CreateUserController.java
+++ src/main/java/com/alucontrol/backendv1/controllers/user/CreateUserController.java
@@ -2,6 +2,7 @@ package com.alucontrol.backendv1.controllers.user;
 
 import com.alucontrol.backendv1.model.UserAccout;
 import com.alucontrol.backendv1.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +21,15 @@ public class CreateUserController {
 
     // Endpoint para criar um novo usuário
     @PostMapping()
-    public ResponseEntity<UserAccout> createUser(@RequestBody UserAccout newUser) {
-        UserAccout createUser = userService.saveNewUser(newUser);
+    public ResponseEntity<UserAccout> createUser(@Valid @RequestParam String username,
+                                                 @Valid @RequestParam String password) {
+
+        UserAccout newUserAccout = new UserAccout();
+        newUserAccout.setUsername(username);
+        newUserAccout.setPassword(password);
+
+        UserAccout createUser = userService.saveNewUser(newUserAccout);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
     }
 }
