f3ca901d982bdc2e90b50920e437b54c523370ab
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-01-24 18:46:37 +0000

    Update include-menu.js
    Issue
    # there is a slight delay on menu load
    
    act:
    -> create a local storage

--- src/main/resources/static/js/modules/templates/include-menu.js
+++ src/main/resources/static/js/modules/templates/include-menu.js
@@ -1,16 +1,26 @@
 //Este método é responsavel por carregar o menu para dentro das páginas
 $(document).ready(function () {
+    //verifica se o menu esta no cache
+    let cachedMenu = localStorage.getItem('menuHTML');
 
-    $.get('/layouts/menu.html')
-        .done(function (data) {
-            $('#menu-sidebar').html(data);
-            setActiveMenu();
-        })
-
-    .fail(function () {
-        console.error('Falha ao carregar o menu');
-    })
+    if(cachedMenu) {
+        $('#menu-sidebar').html(cachedMenu);
+        setActiveMenu();
+    }
+    else {
+        // Faz a requisição AJAX apenas se o menu não estiver no cache
+        $.get('/layouts/menu.html')
+            .done(function (data) {
+                $('#menu-sidebar').html(data);
+                localStorage.setItem('menuHTML', data); // Armazena o menu no cache
+                setActiveMenu();
+            })
+            .fail(function () {
+                console.error('Falha ao carregar o menu');
+            });
+    }
 });
+
 
 //Isto irá destacar o nome dentro da página aberta dentro do menu lateral
 function setActiveMenu () {
